7da5fb2a8cccfbdf702a3d69df9281b22dfcceeb
Jay Clark <jason.clark@raizlabs.com>
2017-08-01 17:10:48 -0400

    Adds some ascii art to help contributers

--- app/FieldofDreams/Model/Fields/Protocols/EndzoneField.swift
+++ app/FieldofDreams/Model/Fields/Protocols/EndzoneField.swift
@@ -5,6 +5,24 @@
 //  Created by Jason Clark on 7/28/17.
 //
 
+/*           EndzoneField
+
+        (p3)-------------(p4)
+         |     endzone     |
+        (p2)-------------(p5)
+         |                 |
+         |     playing     |
+         |      field      |
+         |      proper     |
+         |                 |
+        (p1)-------------(p6)
+   ^     |     endzone     |
+   |    (p0)-------------(p7)
+   |
+    ------>
+
+ */
+
 import Foundation
 
 protocol EndzoneField: Field {
@@ -16,33 +34,39 @@ protocol EndzoneField: Field {
 extension EndzoneField {
 
     var verticies: [Point] {
-        return [
-            (0, 0),
-            (0, endzoneDepth),
-            (width, 0),
-            (width, endzoneDepth),
+        return [p0, p1, p2, p3, p4, p5, p6, p7]
+    }
 
-            (0, endzoneDepth + playingFieldProper),
-            (0, length),
-            (width, endzoneDepth + playingFieldProper),
-            (width, length),
+    var lines: [Line] {
+        return [
+            //sidelines
+            (p0, p3),
+            (p4, p7),
+
+            //baselines
+            (p0, p7),
+            (p3, p4),
+
+            //endzones
+            (p1, p6),
+            (p2, p5)
         ]
     }
+
+}
+
+fileprivate extension EndzoneField {
 
     fileprivate var length: Double {
         return 2 * endzoneDepth + playingFieldProper
     }
 
-    var lines: [Line] {
-        return [
-            (start: (0,0), end: (0, length)), // near sideline
-            (start: (0,0), end: (width, 0)), // near baseline
-            (start: (0, endzoneDepth), end: (width, endzoneDepth)), // near endzone
-
-            (start: (width, 0), end: (width, length)), // far sideline
-            (start: (0, length), end: (width, length)), // far baseline
-            (start: (0, endzoneDepth + playingFieldProper), end: (width, endzoneDepth + playingFieldProper)), // far endzone
-        ]
-    }
-
+    var p0: Point { return (0,     0) }
+    var p1: Point { return (0,     endzoneDepth) }
+    var p2: Point { return (0,     endzoneDepth + playingFieldProper) }
+    var p3: Point { return (0,     length) }
+    var p4: Point { return (width, length) }
+    var p5: Point { return (width, endzoneDepth + playingFieldProper) }
+    var p6: Point { return (width, endzoneDepth) }
+    var p7: Point { return (width, 0) }
 }
