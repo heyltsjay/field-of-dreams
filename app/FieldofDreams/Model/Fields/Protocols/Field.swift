0da607373406f1da5bb9402fcdb7002a3f3b74c3
Jay Clark <jason.clark@raizlabs.com>
2017-08-01 18:20:25 -0400

    abstracts a lot of the field node creation to the field itself

--- app/FieldofDreams/Model/Fields/Protocols/Field.swift
+++ app/FieldofDreams/Model/Fields/Protocols/Field.swift
@@ -5,7 +5,7 @@
 //  Created by Jason Clark on 7/28/17.
 //
 
-import Foundation
+import ARKit
 
 typealias Point = (x: Double, y: Double)
 typealias Line = (start: Point, end: Point)
@@ -14,4 +14,71 @@ protocol Field {
     var unit: UnitLength { get }
     var verticies: [Point] { get }
     var lines: [Line] { get }
+}
+
+extension Field {
+
+    var node: SCNNode {
+        let fieldNode = SCNNode()
+
+        verticies.forEach { vertex in
+            //TODO: Move away from VirtualObject
+            let vertextNode = SCNNode()
+            vertextNode.position = SCNVector3(scaled(vertex.x), 0, scaled(vertex.y))
+
+            // cone
+            vertextNode.addChildNode({
+                let cone = Lamp()
+                cone.loadModel()
+                return cone
+            }())
+
+            // pillar
+            vertextNode.addChildNode({
+                let height = CGFloat(2)
+                let radius = CGFloat(0.02)
+                let cylinder = SCNNode(
+                    geometry: SCNCylinder(radius: radius,
+                                          height: height)
+                )
+                cylinder.pivot = SCNMatrix4MakeTranslation(0, -Float(height / 2), 0)
+                return cylinder
+            }())
+
+            fieldNode.addChildNode(vertextNode)
+        }
+
+        lines.forEach { line in
+            let x1 = scaled(line.start.x)
+            let x2 = scaled(line.end.x)
+            let y1 = scaled(line.start.y)
+            let y2 = scaled(line.end.y)
+            let length = CGFloat(hypot(x1 - x2, y1 - y2))
+            let rotation = atan2(x2 - x1, y2 - y1)
+            let width = CGFloat(0.06)
+            let height = CGFloat(0.01)
+            let lineNode = SCNNode(
+                geometry: SCNBox(width: width,
+                                 height: height,
+                                 length: length,
+                                 chamferRadius: 0)
+            )
+            lineNode.position = SCNVector3(x1, 0, y1)
+            lineNode.rotation = SCNVector4Make(0, 1, 0, Float(rotation))
+            lineNode.pivot = SCNMatrix4MakeTranslation(0, Float(-height/2), Float(-length/2))
+            fieldNode.addChildNode(lineNode)
+        }
+        return fieldNode
+    }
+
+}
+
+extension Field {
+
+    func scaled(_ value: Double) -> Double {
+        return Measurement<UnitLength>(value: value, unit: unit)
+                .converted(to: .meters)
+                .value
+    }
+
 }
--- app/FieldofDreams/Screens/FieldPlacementViewController.swift
+++ app/FieldofDreams/Screens/FieldPlacementViewController.swift
@@ -11,32 +11,14 @@ import SceneKit
 
 class FieldPlacementViewController: ARSceneViewController {
 
-    var field: Field = UltimateFrisbeeField()
-    var cones: [VirtualObject] = []
+    let field: SCNNode = {
+        let field = UltimateFrisbeeField()
+        return field.node
+    }()
 
 }
 
 extension FieldPlacementViewController {
-
-    func addCones() {
-        cones.forEach { cone in
-            cone.removeFromParentNode()
-            cone.unloadModel()
-        }
-        cones = []
-        // Load the content asynchronously.
-        DispatchQueue.global().async {
-            self.cones = self.field.verticies.map { _ in
-                let object = Lamp()
-                object.loadModel()
-                return object
-            }
-
-            DispatchQueue.main.async {
-                self.setNewVirtualObjectPosition(self.focusSquare.lastPosition)
-            }
-        }
-    }
 
     func setNewVirtualObjectPosition(_ pos: SCNVector3?) {
         guard let cameraTransform = sceneView.session.currentFrame?.camera.transform else {
@@ -46,45 +28,8 @@ extension FieldPlacementViewController {
         let cameraWorldPos = SCNVector3.positionFromTransform(cameraTransform)
         let cameraToPosition = pos - cameraWorldPos
         let origin = cameraWorldPos + cameraToPosition
-        cones.enumerated().forEach() { idx, cone in
-            let vertex = field.verticies[idx]
-            let x = Measurement<UnitLength>(value: vertex.x, unit: field.unit).converted(to: .meters).value
-            let y = Measurement<UnitLength>(value: vertex.y, unit: field.unit).converted(to: .meters).value
-            cone.position = origin + SCNVector3(x, 0, y)
-
-            let cylinder: SCNNode = {
-                let geometry = SCNCylinder(radius: 0.02, height: 2)
-                let cylinder = SCNNode(geometry: geometry)
-                cylinder.position = cone.position
-                cylinder.pivot = SCNMatrix4MakeTranslation(0, -Float(geometry.height / 2), 0)
-                return cylinder
-            }()
-
-            sceneView.scene.rootNode.addChildNode(cylinder)
-            if cone.parent == nil {
-                sceneView.scene.rootNode.addChildNode(cone)
-            }
-        }
-
-        field.lines.forEach { line in
-            let (x1, y1, x2, y2) = { () -> (Double, Double, Double, Double) in
-                let scaled = [line.start.x, line.start.y, line.end.x, line.end.y].map {
-                    return Measurement<UnitLength>(value: $0, unit: field.unit).converted(to: .meters).value
-                }
-                return (scaled[0], scaled[1], scaled[2], scaled[3])
-            }()
-
-            let line: SCNNode = {
-                let geometry = SCNBox(width: 0.06, height: 0.01, length: CGFloat(hypot(x1-x2, y1-y2)), chamferRadius: 0)
-                let line = SCNNode(geometry: geometry)
-                line.position = origin + SCNVector3(x1, 0, y1)
-                line.rotation = SCNVector4Make(0, 1, 0, Float(atan2(x2-x1, y2-y1)))
-                line.pivot = SCNMatrix4MakeTranslation(0, Float(-geometry.height/2), Float(-geometry.length/2))
-                return line
-            }()
-
-            sceneView.scene.rootNode.addChildNode(line)
-        }
+        field.position = origin
+        sceneView.scene.rootNode.addChildNode(field)
     }
 
 }
@@ -92,7 +37,7 @@ extension FieldPlacementViewController {
 extension FieldPlacementViewController {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-         addCones()
+         self.setNewVirtualObjectPosition(self.focusSquare.lastPosition)
     }
 
 }
